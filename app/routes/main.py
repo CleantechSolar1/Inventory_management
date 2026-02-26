5609f6f0e16a7652ee8020476b3ee00a6854c35e
Cleantech Solar IT <integration@cleantechsolar.com>
2026-02-26 09:33:48 +0530

    Change asset ID order and update fallback number

--- app/routes/main.py
+++ app/routes/main.py
@@ -228,12 +228,12 @@ def add_item():
             last_asset = (
                 Inventory.query
                 .filter_by(asset_type=form.asset_type.data)
-                .order_by(Inventory.id.asc())  # or order_by asset_tag if more appropriate
+                .order_by(Inventory.id.desc())  # or order_by asset_tag if more appropriate
                 .first()
             )
 
             # Extract numeric suffix
-            START_NUMBER = 485  # fallback start number if no previous asset exists
+            START_NUMBER = 459  # fallback start number if no previous asset exists
             if last_asset:
                 # Get last 4 digits of asset_tag
                 try:
